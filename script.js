882a925e916701747e669e034029692d7924fdcd
Irwan077 <irwanzain85@gmail.com>
2025-11-28 23:29:09 +0800

    update kecil di index.html untuk testing

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const title = document.querySelector("h1");
+    const nav = document.querySelector("nav");
+    const mainContent = document.querySelector("main");
+    const aside = document.querySelector("aside");
+
+    title.textContent = "Belajar JavaScript Dasar - Interaksi Pertama!";
+
+    title.addEventListener("click", () => {
+        nav.classList.toggle("show-nav");
+    });
+
+    document.addEventListener("keydown", (event) => {
+        if (event.key === "i") {
+            document.body.classList.toggle("dark");
+        }
+    });
+
+    let count = 0;
+    mainContent.addEventListener("click", () => {
+        count++;
+        aside.textContent = `Kamu klik konten ini sebanyak ${count} kali`;
+    });
+
+});
